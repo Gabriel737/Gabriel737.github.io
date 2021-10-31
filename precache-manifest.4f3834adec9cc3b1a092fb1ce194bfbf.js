2c343308d31fa9f0a024bd610ee97a4d494bedbe
Gabriel <52950086+Gabriel737@users.noreply.github.com>
2021-10-31 02:34:31 -0700

    Updates

---
+++ precache-manifest.4f3834adec9cc3b1a092fb1ce194bfbf.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9efae9cef378b62275ad029f1c7f9d25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8673417454f211fc7ea",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "5caf162e12e335ee31ba",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "247eb9bc4f7fbac81651",
+    "url": "/static/css/main.ab5f0464.chunk.css"
+  },
+  {
+    "revision": "21c945c819f61a7feb85",
+    "url": "/static/js/2.8a16820f.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.8a16820f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8673417454f211fc7ea",
+    "url": "/static/js/3.b739512d.chunk.js"
+  },
+  {
+    "revision": "5caf162e12e335ee31ba",
+    "url": "/static/js/4.750bf017.chunk.js"
+  },
+  {
+    "revision": "247eb9bc4f7fbac81651",
+    "url": "/static/js/main.b3a23ccc.chunk.js"
+  },
+  {
+    "revision": "6f1b7faf881085478cfd",
+    "url": "/static/js/runtime-main.f6d7c492.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "2dfc188a617cb38312e3b1f8e9941065",
+    "url": "/static/media/AirplaneLogo.2dfc188a.svg"
+  },
+  {
+    "revision": "eeb187bbe276f09b8a2e28015ccb46c5",
+    "url": "/static/media/CECMLogo.eeb187bb.png"
+  },
+  {
+    "revision": "9b9681942fe4b27aea880813abaeb668",
+    "url": "/static/media/CEELogo.9b968194.jpg"
+  },
+  {
+    "revision": "63fe9700235868a5638ee2df67eed635",
+    "url": "/static/media/CadorsLogo.63fe9700.svg"
+  },
+  {
+    "revision": "56ee358ef4fc8492fd3540712550d6e1",
+    "url": "/static/media/ComptiaLogo.56ee358e.png"
+  },
+  {
+    "revision": "f164597fa79eb007f5a959fc90e72142",
+    "url": "/static/media/CourseraLogo.f164597f.png"
+  },
+  {
+    "revision": "39017b36848a6104ba68c7f82a8c25a0",
+    "url": "/static/media/GURWLogo.39017b36.jpg"
+  },
+  {
+    "revision": "a4871e805e7c95f83cb3cc84b776bf1a",
+    "url": "/static/media/GameLogo.a4871e80.svg"
+  },
+  {
+    "revision": "0b4625acc2b8ef6e41e42f42ee05174c",
+    "url": "/static/media/IBMLogo.0b4625ac.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "ac82d984be94f52f19296960255704f1",
+    "url": "/static/media/ProcasLogo.ac82d984.svg"
+  },
+  {
+    "revision": "a9c1e7a7f044f4fce8abaa406bd633f2",
+    "url": "/static/media/TCLogo.a9c1e7a7.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
